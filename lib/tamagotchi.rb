af175cef31adc368e33709cfc96e42fc4caac9b3
steve & leroi <student@epicodus.com>
2015-04-28 16:05:28 -0700

    Get all methods, specs, and time passing.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,7 +1,7 @@
 class Tamagotchi
-  @@current_pet = []  #name, age, health, happiness, hunger, size, type
 
   define_method(:initialize) do |name, type|
+    @dead = false
     @name = name
     @type = type
     @age = 0
@@ -17,10 +17,41 @@ class Tamagotchi
     @size = @size_increment.fetch(@size_rating)    #diminuitive, tiny, small medium, large, huge, gargantuan, colossal
     @last_rest = Time.new(0001, 1, 1)
     @last_rest2 = Time.new(0001, 1, 1)
+    @health_timer = Time.now()
+    @happiness_timer = Time.now()
+    @hunger_timer = Time.now()
+    @age_timer = Time.now()
+  end
+
+  define_method(:update_status) do
+    @current_time = Time.now()
+    @elapsed_health_time = (@current_time - @health_timer).to_i
+    @elapsed_happiness_time = (@current_time - @happiness_timer).to_i
+    @elapsed_hunger_time = (@current_time - @hunger_timer).to_i
+    @age = (@current_time - @age_timer).div(60)
+    if @elapsed_health_time > 30
+      @health -= 5 * @elapsed_health_time.div(30)
+    end
+    if @elapsed_happiness_time > 30
+      @happiness -= 5 * @elapsed_happiness_time.div(30)
+    end
+    if @elapsed_hunger_time > 30
+      @hunger -= 5 * @elapsed_hunger_time.div(30)
+    end
+    if @health <= 0 || @hunger <= 0
+      if @health <= 0
+        @health = 0
+      end
+      if @hunger <= 0
+        @hunger = 0
+      end
+      @dead = true
+      "Your pet has died."
+    end
   end
 
   define_method(:feed) do     # affects health and hunger and size
-    if @hunger < 100
+    if @hunger < 100 && !@dead
       @health += 5
       @hunger += 5
       @size_current += 10
@@ -31,7 +62,7 @@ class Tamagotchi
   end
 
   define_method(:play) do     # affects happiness and hunger
-    if @hunger > 30
+    if @hunger > 30 && !@dead
       @happiness += 5
       @hunger -= 5
     else
@@ -40,13 +71,13 @@ class Tamagotchi
   end
 
   define_method(:love) do   # affects happiness
-    if @happiness < 100
+    if @happiness < 100 && !@dead
       @happiness += 10
     end
   end
 
   define_method(:rest) do   # affects health and size_current
-    if (@size == "tiny" && @size_current < 50) || (@last_rest.min > 10 || @last_rest2.min > 10 )
+    if ((@size == "tiny" && @size_current < 50) && !@dead) || ((@last_rest.min > 10 || @last_rest2.min > 10 ) && !@dead)
       @health += 10
       @size_current += 10
       @last_rest2 = @last_rest
