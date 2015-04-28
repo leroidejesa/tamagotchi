687f2529000f18fdbeb91e4720775bb2fca76147
steve & leroi <student@epicodus.com>
2015-04-28 15:15:16 -0700

    Add additional specs and provide methods for the specs to pass.

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -5,26 +5,61 @@ class Tamagotchi
     @name = name
     @type = type
     @age = 0
-    @health = 100 - rand(20)
-    @happiness = 60 - rand(10)
+    @health = 80
+    @happiness = 60
     @hunger = 50
-    @size = "tiny"    #diminuitive, tiny, small medium, large, huge, gargantuan, colossal
+    @size_current = 0
+    @size_rating = 1
+    @size_increment = { 0 => "diminuitive", 1 => "tiny",
+                        2 => "small", 3 => "medium",
+                        4 => "large", 5 => "huge",
+                        6 => "gargantuan", 7 => "colossal" }
+    @size = @size_increment.fetch(@size_rating)    #diminuitive, tiny, small medium, large, huge, gargantuan, colossal
+    @last_rest = Time.new(0001, 1, 1)
+    @last_rest2 = Time.new(0001, 1, 1)
   end
 
   define_method(:feed) do     # affects health and hunger and size
-
+    if @hunger < 100
+      @health += 5
+      @hunger += 5
+      @size_current += 10
+    end
+    if @size_current >= 100
+      @size_rating += 1
+    end
   end
 
   define_method(:play) do     # affects happiness and hunger
-
+    if @hunger > 30
+      @happiness += 5
+      @hunger -= 5
+    else
+      "your pet is too hungry to play. please feed your pet."
+    end
   end
 
   define_method(:love) do   # affects happiness
-
+    if @happiness < 100
+      @happiness += 10
+    end
   end
 
-  define_method(:rest) do   # affects health and size
-
+  define_method(:rest) do   # affects health and size_current
+    if (@size == "tiny" && @size_current < 50) || (@last_rest.min > 10 || @last_rest2.min > 10 )
+      @health += 10
+      @size_current += 10
+      @last_rest2 = @last_rest
+      @last_rest = Time.new(0001, 1, 1)
+    else
+      "Your pet is not tired. It has rested too recently."
+    end
+    if @health > 100
+      @health = 100
+    end
+    if @size_current >= 100
+      @size_rating += 1
+    end
   end
 
   define_method(:get_name) do
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -10,4 +10,40 @@ describe(Tamagotchi) do
       expect(reptar.get_type()).to(eq("dragon"))
     end
   end
+
+  describe('#feed') do
+    it("feeds pet to a max of 100 hunger rating by increments of 5, also increases health by 5, and size by 10") do
+      reptar = Tamagotchi.new("reptar", "dragon")
+      reptar.feed
+      expect(reptar.get_hunger()).to(eq(55))
+      expect(reptar.get_health()).to(eq(85))
+      expect(reptar.get_size()).to(eq("tiny"))
+    end
+  end
+
+  describe('#play') do
+    it("interacts with pet to increase happiness and decrease hunger bar") do
+      reptar = Tamagotchi.new("reptar", "dragon")
+      reptar.play
+      expect(reptar.get_happiness()).to(eq(65))
+      expect(reptar.get_hunger()).to(eq(45))
+    end
+  end
+
+  describe('#love') do
+    it("interact with pet to increase happiness") do
+      reptar = Tamagotchi.new("reptar", "dragon")
+      reptar.love
+      expect(reptar.get_happiness()).to(eq(70))
+    end
+  end
+
+  describe('#rest') do
+    it("increases health and current size limited to 10 each, up to 2 rests per 10m") do
+      reptar = Tamagotchi.new("reptar", "dragon")
+      reptar.rest
+      expect(reptar.get_health()).to(eq(90))
+      expect(reptar.get_size()).to(eq("tiny"))
+    end
+  end
 end
